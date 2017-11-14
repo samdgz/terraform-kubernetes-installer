78d82c10e656782ff418220d47c0231680dfed30
ddamojip <33328876+ddamojip@users.noreply.github.com>
2017-11-13 19:39:35 -0800

    Update variables.tf

--- instances/k8smaster/variables.tf
+++ instances/k8smaster/variables.tf
@@ -59,3 +59,7 @@ variable "k8s_apiserver_token_admin" {}
 
 # etcd
 variable "etcd_discovery_url" {}
+
+variable "oci_core_image" {
+  default = "ocid1.image.oc1.iad.aaaaaaaahqfv2inssrwfu6arffox3zubxm4swql4hht6t5fk4m5ilfzomgda"
+}
