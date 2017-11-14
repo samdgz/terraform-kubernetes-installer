6c879abd47a59ce1cfbea70ae12b316464b96ec2
ddamojip <33328876+ddamojip@users.noreply.github.com>
2017-11-13 20:21:06 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -314,4 +314,6 @@ variable "worker_docker_device" {
   default = ""
 }
 
-variable "oci_core_image" {}
+variable "oci_core_image" {
+  default="ocid1.image.oc1.iad.aaaaaaaahqfv2inssrwfu6arffox3zubxm4swql4hht6t5fk4m5ilfzomgda"
+  }
