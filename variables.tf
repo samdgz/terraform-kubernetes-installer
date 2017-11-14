8f031c561e5c86cb820ad3c954ebf0ada53aa55a
ddamojip <33328876+ddamojip@users.noreply.github.com>
2017-11-13 20:21:06 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -358,4 +358,6 @@ variable "flannel_backend" {
   default = "VXLAN"
 }
 
-variable "oci_core_image" {}
+variable "oci_core_image" {
+  default="ocid1.image.oc1.iad.aaaaaaaahqfv2inssrwfu6arffox3zubxm4swql4hht6t5fk4m5ilfzomgda"
+  }
