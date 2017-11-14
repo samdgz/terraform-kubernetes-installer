d999ab8c6c84256a406468787cc4ba9fc91f0457
ddamojip <33328876+ddamojip@users.noreply.github.com>
2017-11-13 19:39:10 -0800

    Update variables.tf

--- instances/etcd/variables.tf
+++ instances/etcd/variables.tf
@@ -56,3 +56,7 @@ variable "etcd_iscsi_volume_size" {
   description = "Size of iscsi volume to be created"
   default = 50
 }
+
+variable "oci_core_image" {
+  default = "ocid1.image.oc1.iad.aaaaaaaahqfv2inssrwfu6arffox3zubxm4swql4hht6t5fk4m5ilfzomgda"
+}
