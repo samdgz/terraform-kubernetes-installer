12fdb92b7e7cc05eaf475fe7a8fe045a642f7a9f
ddamojip <33328876+ddamojip@users.noreply.github.com>
2017-11-13 19:26:03 -0800

    Update main.tf

--- instances/k8smaster/main.tf
+++ instances/k8smaster/main.tf
@@ -1,14 +1,17 @@
 /**
  * The instances/k8smaster module provisions and configures one or more Kubernetes Master instances.
  */
-
+     
+/*  image               = "${lookup(data.oci_core_images.ImageOCID.images[0], "id")}"
+*/
+     
 resource "oci_core_instance" "TFInstanceK8sMaster" {
   count               = "${var.count}"
   availability_domain = "${var.availability_domain}"
   compartment_id      = "${var.compartment_ocid}"
   display_name        = "${var.label_prefix}${var.display_name_prefix}-${count.index}"
   hostname_label      = "${var.hostname_label_prefix}-${count.index}"
-  image               = "${lookup(data.oci_core_images.ImageOCID.images[0], "id")}"
+  image               = "${var.oci_core_image}"
   shape               = "${var.shape}"
   subnet_id           = "${var.subnet_id}"
 
