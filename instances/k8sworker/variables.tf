866c619f22d6e12a2d058b6f8ac819d044158903
ddamojip <33328876+ddamojip@users.noreply.github.com>
2017-11-13 19:40:00 -0800

    Update variables.tf

--- instances/k8sworker/variables.tf
+++ instances/k8sworker/variables.tf
@@ -70,3 +70,7 @@ variable "worker_docker_max_log_files" {
 variable "docker_device" {
   default = ""
 }
+
+variable "oci_core_image" {
+  default = "ocid1.image.oc1.iad.aaaaaaaahqfv2inssrwfu6arffox3zubxm4swql4hht6t5fk4m5ilfzomgda"
+}
