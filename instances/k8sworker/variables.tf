ce9bb9694e6030b51a53969955891c067f8fc40e
Hang Du <hang.du@oracle.com>
2017-11-14 20:00:31 -0800

    Update variables.tf

--- instances/k8sworker/variables.tf
+++ instances/k8sworker/variables.tf
@@ -82,6 +82,4 @@ variable "worker_iscsi_volume_mount" {
   default = "/var/lib/docker"
 }
 
-variable "oci_core_image" {
-   default="ocid1.image.oc1.iad.aaaaaaaahqfv2inssrwfu6arffox3zubxm4swql4hht6t5fk4m5ilfzomgda"
-  }
+variable "oci_core_image" {}
