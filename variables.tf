68c153e71c96bf9279fdab7f36743b83166e8e32
Hang Du <hang.du@oracle.com>
2017-11-14 19:57:21 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -313,3 +313,7 @@ variable nat_instance_ad3_enabled {
 variable "worker_docker_device" {
   default = ""
 }
+
+variable "oci_core_image" {
+  default = "ocid1.image.oc1.iad.aaaaaaaahqfv2inssrwfu6arffox3zubxm4swql4hht6t5fk4m5ilfzomgda"
+}
