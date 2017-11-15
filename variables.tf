eb801bdd7c9f09260bd2bcfc72ae19511de008c8
Hang Du <hang.du@oracle.com>
2017-11-14 19:57:21 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -351,3 +351,7 @@ variable "etcd_iscsi_volume_size" {
 variable "oci_core_image" {
   default = "ocid1.image.oc1.iad.aaaaaaaahqfv2inssrwfu6arffox3zubxm4swql4hht6t5fk4m5ilfzomgda"
 }
+
+variable "oci_core_image" {
+  default = "ocid1.image.oc1.iad.aaaaaaaahqfv2inssrwfu6arffox3zubxm4swql4hht6t5fk4m5ilfzomgda"
+}
