7eab3c6ff7e180be82e6aa98a258430835a5743e
Hang Du <hang.du@oracle.com>
2017-11-14 20:00:10 -0800

    Update variables.tf

--- instances/k8smaster/variables.tf
+++ instances/k8smaster/variables.tf
@@ -60,6 +60,4 @@ variable "k8s_apiserver_token_admin" {}
 # etcd
 variable "etcd_discovery_url" {}
 
-variable "oci_core_image" {
-  default="ocid1.image.oc1.iad.aaaaaaaahqfv2inssrwfu6arffox3zubxm4swql4hht6t5fk4m5ilfzomgda"
- }
+variable "oci_core_image" {}
