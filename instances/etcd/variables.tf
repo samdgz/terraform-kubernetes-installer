bdd790b15772b5a7c6727b4f901a898db0803f87
dongguang.zhou@oracle.com <dongguang.zhou@oracle.com>
2017-12-07 18:11:29 -0800

    update etcd bootstrap.template.sh

--- instances/etcd/variables.tf
+++ instances/etcd/variables.tf
@@ -49,3 +49,6 @@ variable "etcd_docker_max_log_files" {
 variable "oci_core_image" {
   default = "ocid1.image.oc1.iad.aaaaaaaahqfv2inssrwfu6arffox3zubxm4swql4hht6t5fk4m5ilfzomgda"
 }
+
+variable "image" {
+}
